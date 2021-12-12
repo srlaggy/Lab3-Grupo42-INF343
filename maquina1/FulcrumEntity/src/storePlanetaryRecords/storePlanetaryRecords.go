fd61b8b80edb70b17b5d2a933f4e3f63d2bdcc50
Darinka <darinka.quinones@sansano.usm.cl>
2021-12-12 01:56:04 -0300

    Funciones Commands en los 3 Fulcrum listos

--- maquina1/FulcrumEntity/src/storePlanetaryRecords/storePlanetaryRecords.go
+++ maquina1/FulcrumEntity/src/storePlanetaryRecords/storePlanetaryRecords.go
@@ -8,13 +8,16 @@ import (
 	"os"
 	"strings"
 	"fmt"
+	"log"
+	"bytes"
+	"bufio"
 )
 
 // Se tendrán las funciones:
 // AddCity
-// UpdateName nombre planeta nombre ciudad nuevo valor
-// UpdateNumber nombre planeta nombre ciudad nuevo valor
-// DeleteCity nombre planeta nombre ciudad
+// UpdateName
+// UpdateNumber
+// DeleteCity
 
 // var global
 var listRegister []ut.Register
@@ -23,12 +26,18 @@ func Comands(str string) {
 	s := strings.Split(str, " ")
 	if (s[0] == "AddCity"){
 		AddCity(s[1:])
+	} else if (s[0] == "UpdateName"){
+		UpdateName(s[1:])
+	} else if (s[0] == "UpdateNumber"){
+		UpdateNumber(s[1:])
+	} else if (s[0] == "DeleteCity"){
+		DeleteCity(s[1:])
 	}
 }
 
 // AddCity nombre_planeta nombre_ciudad [nuevo valor]
 //              s[0]          s[1]           s[2]
-func AddCity(s []string){
+func AddCity(s []string) {
 	var fileName string = "utils/RegistroPlanetario/" + s[0] + ".txt"
 
 	// Condición para verificar si el planeta existe o no:
@@ -64,6 +73,163 @@ func AddCity(s []string){
 	fmt.Println("lista: ", listRegister)
 }
 
+
+// UpdateName nombre_planeta nombre_ciudad [nuevo valor]
+//                s[0]          s[1]           s[2]
+func UpdateName(s []string) {
+	var fileName string = "utils/RegistroPlanetario/" + s[0] + ".txt"
+	f, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	var bs []byte
+	buf := bytes.NewBuffer(bs)
+
+	scanner := bufio.NewScanner(f)
+	var cantRebels string
+	var scannerSplit []string
+	for scanner.Scan() {
+		scannerSplit = strings.Split(scanner.Text(), " ")
+		if scannerSplit[0] != s[1] {
+			_, err := buf.Write(scanner.Bytes())
+			if err != nil {
+				log.Fatal(err)
+			}
+			_, err = buf.WriteString("\n")
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else{
+			cantRebels = scannerSplit[1]
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	err = os.WriteFile(fileName, buf.Bytes(), 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	// Agregar el dato de la ciudad
+	file, error1 := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	ut.FailOnError(error1, "Failed to open file")
+	_, error2 := file.WriteString(s[2] + " " + cantRebels + "\n")
+	ut.FailOnError(error2, "Failed to write file")
+	defer file.Close()
+	
+	// Cambiar el número en reloj vectorial del planeta	
+	for i := 0; i < len(listRegister); i++ {
+		if (listRegister[i].NamePlanet == s[0]){
+			listRegister[i].RelojPlanet.Server1 = listRegister[i].RelojPlanet.Server1 + 1
+		}
+	}
+	fmt.Println("lista: ", listRegister)
+}
+
+
+// UpdateNumber nombre_planeta nombre_ciudad [nuevo valor]
+//                  s[0]          s[1]           s[2]
+func UpdateNumber(s []string) {
+	var fileName string = "utils/RegistroPlanetario/" + s[0] + ".txt"
+	f, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	var bs []byte
+	buf := bytes.NewBuffer(bs)
+
+	scanner := bufio.NewScanner(f)
+	var scannerSplit []string
+	for scanner.Scan() {
+		scannerSplit = strings.Split(scanner.Text(), " ")
+		if scannerSplit[0] != s[1] {
+			_, err := buf.Write(scanner.Bytes())
+			if err != nil {
+				log.Fatal(err)
+			}
+			_, err = buf.WriteString("\n")
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	err = os.WriteFile(fileName, buf.Bytes(), 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	// Agregar el dato de la ciudad
+	file, error1 := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	ut.FailOnError(error1, "Failed to open file")
+	_, error2 := file.WriteString(s[1] + " " + s[2] + "\n")
+	ut.FailOnError(error2, "Failed to write file")
+	defer file.Close()
+	
+	// Cambiar el número en reloj vectorial del planeta	
+	for i := 0; i < len(listRegister); i++ {
+		if (listRegister[i].NamePlanet == s[0]){
+			listRegister[i].RelojPlanet.Server1 = listRegister[i].RelojPlanet.Server1 + 1
+		}
+	}
+	fmt.Println("lista: ", listRegister)
+}
+
+
+// DeleteCity nombre_planeta nombre_ciudad
+//                 s[0]          s[1]
+func DeleteCity(s []string) {
+	var fileName string = "utils/RegistroPlanetario/" + s[0] + ".txt"
+	f, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	var bs []byte
+	buf := bytes.NewBuffer(bs)
+
+	scanner := bufio.NewScanner(f)
+	var scannerSplit []string
+	for scanner.Scan() {
+		scannerSplit = strings.Split(scanner.Text(), " ")
+		if scannerSplit[0] != s[1] {
+			_, err := buf.Write(scanner.Bytes())
+			if err != nil {
+				log.Fatal(err)
+			}
+			_, err = buf.WriteString("\n")
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	err = os.WriteFile(fileName, buf.Bytes(), 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	// Cambiar el número en reloj vectorial del planeta	
+	for i := 0; i < len(listRegister); i++ {
+		if (listRegister[i].NamePlanet == s[0]){
+			listRegister[i].RelojPlanet.Server1 = listRegister[i].RelojPlanet.Server1 + 1
+		}
+	}
+	fmt.Println("lista: ", listRegister)
+}
+
+
+// Funcion GetListRegister
 func GetListRegister() []ut.Register{
 	return listRegister
 }
